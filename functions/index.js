6dda8a97e925a4532f2a434bb60a31336540c2fa
sakal <sakals@mail.ua>
2018-06-18 09:34:44 -0400

    feat: firebase function body: upload file via endPoint with renaming and resizing
    Signed-off-by: sakal <sakals@mail.ua>

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,28 @@
 const functions = require('firebase-functions');
-const gcs = require('@google-cloud/storage')();
+
+const gcsConfig = {
+  projectId: 'sakal-s',
+  keyFilename: 'sakal-s-firebase-adminsdk-tttz9-5022f7cb73.json',
+};
+
+const gcs = require('@google-cloud/storage')(gcsConfig);
+
 const spawn = require('child-process-promise').spawn;
+const cors = require('cors')({origin: true});
+const Busboy = require('busboy');
 
 // node modules
 const os = require('os');
+const fs = require('fs');
 const path = require('path');
 
-exports.onFileChange = functions.storage.object().onFinalize(event => {
+// Upload Img and with endPoint rename + resize
+//endPoint: https://us-central1-sakal-s.cloudfunctions.net/uploadFile
+exports.onUpload = functions.storage.object().onFinalize(event => {
   const {bucket, contentType, name: filePath} = event;
-  console.log('File changed');
 
   if (path.basename(filePath).startsWith('resized-')) {
-    console.log('Already renamed !!!');
-
+    console.log('Already resized');
     return null;
   }
 
@@ -30,5 +40,50 @@ exports.onFileChange = functions.storage.object().onFinalize(event => {
           metaData,
         })
       });
+  });
+});
+
+exports.uploadFile = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    if (req.method !== 'POST') {
+      return res.status(200).json({
+        message: 'Not allowed 👿'
+      })
+    }
+
+    const busboy = new Busboy({headers: req.headers});
+    let uploadData = null;
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+      const filepath = path.join(os.tmpdir(), filename);
+
+      uploadData = {file: filepath, type: mimetype};
+      file.pipe(fs.createWriteStream(filepath));
+    });
+
+    busboy.on('finish', () => {
+      const bucket = gcs.bucket('sakal-s.appspot.com');
+
+      bucket.upload(uploadData.file, {
+        uploadType: 'media',
+        metadata: {
+          metadata: {
+            contentType: uploadData.type,
+          }
+        },
+      })
+        .then(() => {
+          res.status(200).json({
+            message: 'Uploaded file successfully ✨'
+          })
+        })
+        .catch(err => {
+          res.status(500).json({
+            error: err,
+          })
+        })
+    });
+
+    busboy.end(req.rawBody);
   });
 });
